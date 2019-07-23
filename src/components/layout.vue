f211f4afaffa40f42dc8aac8fe2640f805b5e0ec
jiuanli666 <2069420645@qq.com>
2019-07-23 18:06:55 +0800

    jiuwanli666

---
+++ src/components/layout.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<div class="app-head">
+			<div class="app-head-inner">
+				<img src="../assets/logo.png" alt="">
+				<div class="head-nav">
+					<ul class="nav-list">
+						<li>登录</li>
+						<li class="nav-pile">|</li>
+						<li>注册</li>
+						<li class="nav-pile">|</li>
+						<li>关于</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="app-content">content</div>
+		<div class="app-foot">
+			<p>@ 2016 fis</p>
+		</div>
+	</div>
+
+</template>
+<script type="text/javascript">
+	
+export default {
+	data () {
+		return {
+
+		}
+	},
+	methods: {
+
+	}
+}
+
+
+</script>
+<style>
+/* 
+Layout 可以把reset样式写在此处。
+	要注意的是，style不可以添加scoped，
+	那样html、body和vue实例挂载的节点样式就不会被写。
+
+ */
+
+</style>
